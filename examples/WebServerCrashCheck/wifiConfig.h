d3f17936102a69a23186bbddee5a9f0370013e0d
Jonas Scharpf <jonas@brainelectronics.de>
2020-01-09 15:38:55 +0100

    untracked wifiConfig.h file

--- examples/WebServerCrashCheck/wifiConfig.h
+++
@@ -1,7 +0,0 @@
-// enter the credentials of your network
-const char* ssid = "********";
-const char* password = "********";
-
-// create an access point with a custom name and password
-const char* accessPointSsid = "********";
-const char* accessPointPassword = "********";
